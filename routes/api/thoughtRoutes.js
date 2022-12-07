5bced11862af395e50a882efdafa000a50edae36
hburnton <hburnton@gmail.com>
2022-12-07 09:43:29 -0700

    adjust routes for legibility

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -9,10 +9,20 @@ const {
     deleteReaction,
 } = require('../../controllers/thoughtController')
 
-router.route('/').get(getThoughts).post(createThought);
+router
+    .route('/')
+    .get(getThoughts)
+    .post(createThought);
 
-router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+router
+    .route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
 
-router.route(':/thoughtId/reactions').post(createReaction).delete(deleteReaction);
+router
+    .route(':/thoughtId/reactions')
+    .post(createReaction)
+    .delete(deleteReaction);
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -10,10 +10,20 @@ const {
     deleteFriend
 } = require('../../controllers/userController');
 
-router.route('/').get(getUsers).post(createUser);
+router
+    .route('/')
+    .get(getUsers)
+    .post(createUser);
 
-router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+router
+    .route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
 
-router.route('/:userId/friends/:friendId').post(createFriend).delete(deleteFriend);
+router
+    .route('/:userId/friends/:friendId')
+    .post(createFriend)
+    .delete(deleteFriend);
 
 module.exports = router;
